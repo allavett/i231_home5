99fde030078c4b867a867d58088d4ec435dfb1d6
Allar Vendla <allar.vendla@playtech.com>
2018-04-10 14:48:08 +0300

    Add initial idea for building Nodes from perfect string.

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,16 @@
+/*
+ * Write a method to build a tree from the right parenthetic string representation (return the root node of the tree)
+ * and a method to construct the left parenthetic string representation of a tree represented by the root node this.
+ * Tree is a pointer structure with two pointers to link nodes of type Node - pointer to the first child and pointer to
+ * the next sibling. Build test trees and print the results in main-method, do not forget to test a tree that consists
+ * of one node only. Node name must be non-empty and must not contain round brackets, commas and whitespace symbols. In
+ * case of an invalid input string the parsePostfix method must throw a RuntimeException with meaningful error message.
+ *
+ * Used materials:
+ * http://enos.itcollege.ee/~jpoial/algoritmid/puud.html
+ * https://stackoverflow.com/questions/4662215/how-to-extract-a-substring-using-regex
+ */
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
 import java.util.*;
 
@@ -7,12 +20,81 @@ public class Node {
    private Node firstChild;
    private Node nextSibling;
 
+
    Node (String n, Node d, Node r) {
-      // TODO!!! Your constructor here
+      setName(n);
+      setFirstChild(d);
+      setNextSibling(r);
    }
-   
+
+   public String getName() {
+      return name;
+   }
+
+   private void setName(String name) {
+      this.name = name;
+   }
+
+   public Node getFirstChild() {
+      return firstChild;
+   }
+
+   private void setFirstChild(Node firstChild) {
+      this.firstChild = firstChild;
+   }
+
+   public Node getNextSibling() {
+      return nextSibling;
+   }
+
+   private void setNextSibling(Node nextSibling) {
+      this.nextSibling = nextSibling;
+   }
+
+   //Recursive method that splits string and creates nodes
    public static Node parsePostfix (String s) {
-      return null;  // TODO!!! return the root
+      if (s.isEmpty()) return null;
+      char[] charArray = s.toCharArray();
+      StringBuilder currentBuffer = new StringBuilder();
+      StringBuilder siblingBuffer = new StringBuilder();
+      StringBuilder childBuffer = new StringBuilder();
+      ArrayList<String> siblings = new ArrayList<>();
+      Boolean hasChild = false;
+      int parenthesisPairCount = 0;
+      for (int i = 0; i < charArray.length; i++) {
+         if (String.valueOf(charArray[i]).equals("(") && !hasChild) {
+            hasChild = true;
+            parenthesisPairCount++;
+         }else if (hasChild){
+            if (String.valueOf(charArray[i]).equals("(")){
+               parenthesisPairCount++;
+            }else if (String.valueOf(charArray[i]).equals(")")){
+               parenthesisPairCount--;
+            }
+            if (parenthesisPairCount <= 0){
+               hasChild = false;
+            } else {
+               childBuffer.append(charArray[i]);
+            }
+         }else{
+            if (String.valueOf(charArray[i]).equals(",")){
+               for (int j = i+1; j < charArray.length; j++) {
+                  siblingBuffer.append(charArray[j]);
+               }
+               break; // break out from loop (finish)
+            }
+            currentBuffer.append(charArray[i]);
+         }
+      }
+      String currentNode = currentBuffer.toString();
+      String childNode = childBuffer.toString();
+      String siblingNode = siblingBuffer.toString();
+
+      return new Node( currentNode, parsePostfix(childNode), parsePostfix(siblingNode));  // TODO!!! return the root
+   }
+
+   private void createNode(String s){
+
    }
 
    public String leftParentheticRepresentation() {
@@ -20,10 +102,11 @@ public class Node {
    }
 
    public static void main (String[] param) {
-      String s = "(B1,C)A";
+      String s = "A(B,(B2)C),B3(A2,C2)";
       Node t = Node.parsePostfix (s);
       String v = t.leftParentheticRepresentation();
       System.out.println (s + " ==> " + v); // (B1,C)A ==> A(B1,C)
    }
+
 }
 
